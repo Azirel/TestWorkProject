5fe51d8dbf6a81eb041211b371b7bc8f693585b7
Andrey Sudakov <azirel92@gmail.com>
2015-10-20 06:34:16 +0300

    working DrawAreas

---
+++ Assets/MyScripts/CursorManagerScript.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CursorManagerScript : MonoBehaviour
+{
+    Vector3 pos;
+    LineRenderer line;
+    public int delays = 0;
+    //public UnityEngine.UI.Text text;
+    public float delayTime;
+    List<TracePoint> pointsList;
+    TracePoint tempPoint;
+    //public Text pointsCountViewer;
+    //public Text vertexesCountViewer;
+    int timer = 0;
+    public float startthinckness;
+    public float endthinckness;
+    class TracePoint
+    {
+        public Vector3 position;
+        public int delaysCount;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.SetWidth(startthinckness, endthinckness);
+        pointsList = new List<TracePoint>();
+        //line.SetVertexCount(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void FixedUpdate()
+    {
+        //pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0;
+        //gameObject.transform.position = pos;
+        if (Input.GetMouseButton(0))
+        {
+            AddPoint(pos);
+            line.enabled = true;
+            line.SetPosition(0, pos);
+        }
+        ModifyLine();
+        if (pointsList.Count > 0)
+        {
+            //pointsCountViewer.text = pointsList.Count.ToString();
+            StartCoroutine("LastPointsEliminator");
+        }
+        else line.enabled = false;
+        
+    }
+    void TextUpdate()
+    {
+    }
+    void AddPoint(Vector3 pointPos)
+    {
+        tempPoint = new TracePoint();
+
+        tempPoint.position = pointPos;
+        tempPoint.delaysCount = delays;
+
+        if (!pointsList.Contains(tempPoint))
+        {
+            pointsList.Insert(0, tempPoint);
+        }
+    }
+    void ModifyLine()
+    {
+        line.SetVertexCount(pointsList.Count);
+        //Debug.Log(pointsList.Count);
+        for (int i = 1; i < pointsList.Count; i++)
+        {
+            line.SetPosition(i, pointsList[i].position);
+        }
+    }
+    IEnumerator LastPointsEliminator()
+    {
+        foreach (var item in pointsList)
+        {
+            item.delaysCount--;
+            if (item.delaysCount < 1)
+                pointsList.Remove(item);
+        }
+        yield return new WaitForSeconds(delayTime);
+    }
+}
---
+++ Assets/MyScripts/MenuSwitcher.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSwitcher : MonoBehaviour
+{
+
+    public GameObject game;
+    public GameObject startMenu;
+    public GameObject additionMenu;
+    public GameObject drawArea;
+    CursorManagerScript cms;
+    GeneralManagment gm;
+
+    // Use this for initialization
+    void Start()
+    {
+        cms = GetComponent<CursorManagerScript>();
+        gm = GetComponent<GeneralManagment>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void StartMenuEnable()
+    {
+        startMenu.SetActive(true);
+        game.SetActive(false);
+        additionMenu.SetActive(false);
+        cms.enabled = false;
+        gm.isInStartMenu = true;
+        gm.isInGame = false;
+        gm.isInAddition = false;
+        drawArea.SetActive(false);
+    }
+    public void AdditionMenuEnable()
+    {
+        startMenu.SetActive(false);
+        game.SetActive(false);
+        additionMenu.SetActive(true);
+        cms.enabled = false;
+        gm.isInStartMenu = false;
+        gm.isInGame = false;
+        gm.isInAddition = true;
+        drawArea.SetActive(true);
+    }
+    public void Game()
+    {
+        startMenu.SetActive(false);
+        game.SetActive(true);
+        additionMenu.SetActive(false);
+        cms.enabled = true;
+        gm.isInStartMenu = false;
+        gm.isInGame = true;
+        gm.isInAddition = false;
+        gm.drawArea.gameObject.SetActive(true);
+        drawArea.SetActive(true);
+    }
+}
